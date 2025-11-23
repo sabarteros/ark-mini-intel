4b5b42814104a73ff1d492bcf0e534a281a62656
sabar <139615515+sabarteros@users.noreply.github.com>
2025-11-23 10:11:28 +0700

    Update cache.ts

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,1 +1,17 @@
+import { Redis } from "@upstash/redis";
 
+const url = process.env.UPSTASH_REDIS_REST_URL;
+const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+let redis: Redis | null = null;
+if (url && token) redis = new Redis({ url, token });
+
+export async function cacheGet(key: string) {
+  if (redis) return redis.get(key);
+  return null;
+}
+
+export async function cacheSet(key: string, value: any, ttl = 60) {
+  if (redis) return redis.set(key, value, { ex: ttl });
+  return null;
+}
